aebb6706ee777fa5e72034f435d7f53ddd30f896
sanja <sanja@DESKTOP-6S68U6G>
2023-12-10 19:06:27 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+
+            // Логика проверки введенных учетных данных - добавьте здесь свою собственную логику
+
+            if (IsValidLogin(username, password))
+            {
+                MessageBox.Show("Вход выполнен успешно!");
+                Close(); // Закрывает текущее окно (LoginWindow)
+            }
+            else
+            {
+                MessageBox.Show("Неверные учетные данные. Попробуйте снова.");
+            }
+        }
+
+        private bool IsValidLogin(string username, string password)
+        {
+            // Добавьте свою логику проверки соответствия введенных учетных данных вашим требованиям
+            // Здесь возвращается фиктивное значение true для демонстрационных целей
+            return true;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+namespace YourNamespace
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            string middleName = middleNameTextBox.Text;
+            string city = cityTextBox.Text;
+            DateTime birthDate = birthDateDatePicker.SelectedDate ?? DateTime.MinValue;
+            string username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+            string email = emailTextBox.Text;
+
+            // Регистрационная логика - добавьте здесь код для сохранения данных в базу данных или другие операции
+
+            MessageBox.Show("Вы успешно зарегистрированы!");
+            ResetFields();
+
+            // Открываем новое окно входа
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            Close();
+        }
+
+        private void ResetFields()
+        {
+            firstNameTextBox.Text = string.Empty;
+            lastNameTextBox.Text = string.Empty;
+            middleNameTextBox.Text = string.Empty;
+            cityTextBox.Text = string.Empty;
+            birthDateDatePicker.SelectedDate = null;
+            usernameTextBox.Text = string.Empty;
+            passwordBox.Password = string.Empty;
+            emailTextBox.Text = string.Empty;
+        }
+    }
+}
